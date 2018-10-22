ba8b3321cf9e07d482f3f7fc2bdf090200515ca7
Peter M <petermogyoros@gmail.com>
2018-10-22 17:20:40 +0100

    in process of creating the SQL view

--- dolcegusto/models.py
+++ dolcegusto/models.py
@@ -27,11 +27,98 @@ class DolceGusto_table(models.Model):
 
     # bypassing django model and querying SQL directely
 def scrap_rate(self):
-    with connection.cursor() as cursor:
-        cursor.execute("SELECT avg(combined_side_a_ng), avg(combined_side_b_ng), avg(combined_side_a_re), avg(combined_side_b_re) FROM dolcegusto_dolcegusto_table")
-        row = cursor.fetchone()
-    return row
 
-    # def __str__(self):
-    #
-    #     return str(mean(self.a_ok))
+    with connection.cursor() as cursor:
+        cursor.execute(
+        "SELECT (SELECT avg(combined_side_a_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime >= TIMESTAMP 'today') combined_side_a_ng_avg_1, (SELECT avg(combined_side_b_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime >= TIMESTAMP 'today') combined_side_b_ng_avg_1, (SELECT avg(combined_side_a_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime >= TIMESTAMP 'today') combined_side_a_re_avg_1, (SELECT avg(combined_side_b_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime >= TIMESTAMP 'today') combined_side_b_re_avg_1, (SELECT avg(combined_side_a_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '1 day' AND csv_datetime < CURRENT_DATE - INTERVAL '0 day') combined_side_a_ng_avg_2, (SELECT avg(combined_side_b_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime >
+         CURRENT_DATE - INTERVAL '1 day' AND csv_datetime < CURRENT_DATE - INTERVAL '0 day') combined_side_b_ng_avg_2, (SELECT avg(combined_side_a_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime >
+         CURRENT_DATE - INTERVAL '1 day' AND csv_datetime < CURRENT_DATE - INTERVAL '0 day') combined_side_a_re_avg_2, (SELECT avg(combined_side_b_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime >
+         CURRENT_DATE - INTERVAL '1 day' AND csv_datetime < CURRENT_DATE - INTERVAL '0 day') combined_side_b_re_avg_2, (SELECT avg(combined_side_a_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime >
+         CURRENT_DATE - INTERVAL '2 day' AND csv_datetime < CURRENT_DATE - INTERVAL '1 day') combined_side_a_ng_avg_3, (SELECT avg(combined_side_b_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime >
+         CURRENT_DATE - INTERVAL '2 day' AND csv_datetime < CURRENT_DATE - INTERVAL '1 day') combined_side_b_ng_avg_3, (SELECT avg(combined_side_a_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime >
+         CURRENT_DATE - INTERVAL '2 day' AND csv_datetime < CURRENT_DATE - INTERVAL '1 day') combined_side_a_re_avg_3, (SELECT avg(combined_side_b_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime >
+         CURRENT_DATE - INTERVAL '2 day' AND csv_datetime < CURRENT_DATE - INTERVAL '1 day') combined_side_b_re_avg_3,
+
+-- query for three days ago
+(SELECT avg(combined_side_a_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '3 day' AND csv_datetime < CURRENT_DATE - INTERVAL '2 day') combined_side_a_ng_avg_4,
+(SELECT avg(combined_side_b_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '3 day' AND csv_datetime < CURRENT_DATE - INTERVAL '2 day') combined_side_b_ng_avg_4,
+(SELECT avg(combined_side_a_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '3 day' AND csv_datetime < CURRENT_DATE - INTERVAL '2 day') combined_side_a_re_avg_4,
+(SELECT avg(combined_side_b_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '3 day' AND csv_datetime < CURRENT_DATE - INTERVAL '2 day') combined_side_b_re_avg_4,
+
+-- query for four days ago
+(SELECT avg(combined_side_a_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '4 day' AND csv_datetime < CURRENT_DATE - INTERVAL '3 day') combined_side_a_ng_avg_5,
+(SELECT avg(combined_side_b_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '4 day' AND csv_datetime < CURRENT_DATE - INTERVAL '3 day') combined_side_b_ng_avg_5,
+(SELECT avg(combined_side_a_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '4 day' AND csv_datetime < CURRENT_DATE - INTERVAL '3 day') combined_side_a_re_avg_5,
+(SELECT avg(combined_side_b_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '4 day' AND csv_datetime < CURRENT_DATE - INTERVAL '3 day') combined_side_b_re_avg_5,
+
+-- query for five days ago
+(SELECT avg(combined_side_a_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '5 day' AND csv_datetime < CURRENT_DATE - INTERVAL '4 day') combined_side_a_ng_avg_6,
+(SELECT avg(combined_side_b_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '5 day' AND csv_datetime < CURRENT_DATE - INTERVAL '4 day') combined_side_b_ng_avg_6,
+(SELECT avg(combined_side_a_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '5 day' AND csv_datetime < CURRENT_DATE - INTERVAL '4 day') combined_side_a_re_avg_6,
+(SELECT avg(combined_side_b_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '5 day' AND csv_datetime < CURRENT_DATE - INTERVAL '4 day') combined_side_b_re_avg_6,
+
+-- query for six days ago
+(SELECT avg(combined_side_a_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '6 day' AND csv_datetime < CURRENT_DATE - INTERVAL '5 day') combined_side_a_ng_avg_7,
+(SELECT avg(combined_side_b_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '6 day' AND csv_datetime < CURRENT_DATE - INTERVAL '5 day') combined_side_b_ng_avg_7,
+(SELECT avg(combined_side_a_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '6 day' AND csv_datetime < CURRENT_DATE - INTERVAL '5 day') combined_side_a_re_avg_7,
+(SELECT avg(combined_side_b_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '6 day' AND csv_datetime < CURRENT_DATE - INTERVAL '5 day') combined_side_b_re_avg_7
+"
+        )
+        today = cursor.fetchone()
+    return today
+
+# SQL View query backup:
+# CREATE OR REPLACE VIEW past_week (
+# 	combined_side_a_ng_avg_1, combined_side_b_ng_avg_1, combined_side_a_re_avg_1, combined_side_b_re_avg_1,
+# 	combined_side_a_ng_avg_2, combined_side_b_ng_avg_2, combined_side_a_re_avg_2, combined_side_b_re_avg_2,
+# 	combined_side_a_ng_avg_3, combined_side_b_ng_avg_3, combined_side_a_re_avg_3, combined_side_b_re_avg_3,
+# 	combined_side_a_ng_avg_4, combined_side_b_ng_avg_4, combined_side_a_re_avg_4, combined_side_b_re_avg_4,
+# 	combined_side_a_ng_avg_5, combined_side_b_ng_avg_5, combined_side_a_re_avg_5, combined_side_b_re_avg_5,
+# 	combined_side_a_ng_avg_6, combined_side_b_ng_avg_6, combined_side_a_re_avg_6, combined_side_b_re_avg_6,
+# 	combined_side_a_ng_avg_7, combined_side_b_ng_avg_7, combined_side_a_re_avg_7, combined_side_b_re_avg_7
+# )
+#
+# AS SELECT
+# -- query for today
+# (SELECT avg(combined_side_a_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime >= TIMESTAMP 'today'),
+# (SELECT avg(combined_side_b_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime >= TIMESTAMP 'today'),
+# (SELECT avg(combined_side_a_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime >= TIMESTAMP 'today'),
+# (SELECT avg(combined_side_b_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime >= TIMESTAMP 'today'),
+#
+# -- query for one day ago
+# (SELECT avg(combined_side_a_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '1 day' AND csv_datetime < CURRENT_DATE - INTERVAL '0 day'),
+# (SELECT avg(combined_side_b_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '1 day' AND csv_datetime < CURRENT_DATE - INTERVAL '0 day'),
+# (SELECT avg(combined_side_a_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '1 day' AND csv_datetime < CURRENT_DATE - INTERVAL '0 day'),
+# (SELECT avg(combined_side_b_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '1 day' AND csv_datetime < CURRENT_DATE - INTERVAL '0 day'),
+#
+# -- query for two days ago
+# (SELECT avg(combined_side_a_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '2 day' AND csv_datetime < CURRENT_DATE - INTERVAL '1 day'),
+# (SELECT avg(combined_side_b_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '2 day' AND csv_datetime < CURRENT_DATE - INTERVAL '1 day'),
+# (SELECT avg(combined_side_a_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '2 day' AND csv_datetime < CURRENT_DATE - INTERVAL '1 day'),
+# (SELECT avg(combined_side_b_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '2 day' AND csv_datetime < CURRENT_DATE - INTERVAL '1 day'),
+#
+# -- query for three days ago
+# (SELECT avg(combined_side_a_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '3 day' AND csv_datetime < CURRENT_DATE - INTERVAL '2 day'),
+# (SELECT avg(combined_side_b_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '3 day' AND csv_datetime < CURRENT_DATE - INTERVAL '2 day'),
+# (SELECT avg(combined_side_a_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '3 day' AND csv_datetime < CURRENT_DATE - INTERVAL '2 day'),
+# (SELECT avg(combined_side_b_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '3 day' AND csv_datetime < CURRENT_DATE - INTERVAL '2 day'),
+#
+# -- query for four days ago
+# (SELECT avg(combined_side_a_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '4 day' AND csv_datetime < CURRENT_DATE - INTERVAL '3 day'),
+# (SELECT avg(combined_side_b_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '4 day' AND csv_datetime < CURRENT_DATE - INTERVAL '3 day'),
+# (SELECT avg(combined_side_a_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '4 day' AND csv_datetime < CURRENT_DATE - INTERVAL '3 day'),
+# (SELECT avg(combined_side_b_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '4 day' AND csv_datetime < CURRENT_DATE - INTERVAL '3 day'),
+#
+# -- query for five days ago
+# (SELECT avg(combined_side_a_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '5 day' AND csv_datetime < CURRENT_DATE - INTERVAL '4 day'),
+# (SELECT avg(combined_side_b_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '5 day' AND csv_datetime < CURRENT_DATE - INTERVAL '4 day'),
+# (SELECT avg(combined_side_a_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '5 day' AND csv_datetime < CURRENT_DATE - INTERVAL '4 day'),
+# (SELECT avg(combined_side_b_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '5 day' AND csv_datetime < CURRENT_DATE - INTERVAL '4 day'),
+#
+# -- query for six days ago
+# (SELECT avg(combined_side_a_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '6 day' AND csv_datetime < CURRENT_DATE - INTERVAL '5 day'),
+# (SELECT avg(combined_side_b_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '6 day' AND csv_datetime < CURRENT_DATE - INTERVAL '5 day'),
+# (SELECT avg(combined_side_a_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '6 day' AND csv_datetime < CURRENT_DATE - INTERVAL '5 day'),
+# (SELECT avg(combined_side_b_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '6 day' AND csv_datetime < CURRENT_DATE - INTERVAL '5 day')
+#
+# ;
