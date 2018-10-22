5ce89e95d834a5cfc962e029b3e0b41af2adb64b
Peter <petermogyoros@yahoo.co.uk>
2018-10-22 22:07:52 +0100

    Postgres views development

--- dolcegusto/models.py
+++ dolcegusto/models.py
@@ -1,4 +1,4 @@
-from django.db import models, connection
+from django.db import models
 
 class DolceGusto_table(models.Model):
     csv_datetime = models.DateTimeField(
@@ -25,47 +25,58 @@ class DolceGusto_table(models.Model):
     b_side_re = models.IntegerField()
 
 
-    # bypassing django model and querying SQL directely
-def scrap_rate(self):
+class Past_Week(models.Model):
+    id = models.IntegerField(primary_key = True)
+    combined_side_a_ng_avg_1 = models.DecimalField(max_digits=22, decimal_places=16)
+    combined_side_b_ng_avg_1 = models.DecimalField(max_digits=22, decimal_places=16)
+    combined_side_a_re_avg_1 = models.DecimalField(max_digits=22, decimal_places=16)
+    combined_side_b_re_avg_1 = models.DecimalField(max_digits=22, decimal_places=16)
 
-    with connection.cursor() as cursor:
-        cursor.execute(
-        "SELECT (SELECT avg(combined_side_a_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime >= TIMESTAMP 'today') combined_side_a_ng_avg_1, (SELECT avg(combined_side_b_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime >= TIMESTAMP 'today') combined_side_b_ng_avg_1, (SELECT avg(combined_side_a_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime >= TIMESTAMP 'today') combined_side_a_re_avg_1, (SELECT avg(combined_side_b_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime >= TIMESTAMP 'today') combined_side_b_re_avg_1, (SELECT avg(combined_side_a_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '1 day' AND csv_datetime < CURRENT_DATE - INTERVAL '0 day') combined_side_a_ng_avg_2, (SELECT avg(combined_side_b_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime >
-         CURRENT_DATE - INTERVAL '1 day' AND csv_datetime < CURRENT_DATE - INTERVAL '0 day') combined_side_b_ng_avg_2, (SELECT avg(combined_side_a_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime >
-         CURRENT_DATE - INTERVAL '1 day' AND csv_datetime < CURRENT_DATE - INTERVAL '0 day') combined_side_a_re_avg_2, (SELECT avg(combined_side_b_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime >
-         CURRENT_DATE - INTERVAL '1 day' AND csv_datetime < CURRENT_DATE - INTERVAL '0 day') combined_side_b_re_avg_2, (SELECT avg(combined_side_a_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime >
-         CURRENT_DATE - INTERVAL '2 day' AND csv_datetime < CURRENT_DATE - INTERVAL '1 day') combined_side_a_ng_avg_3, (SELECT avg(combined_side_b_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime >
-         CURRENT_DATE - INTERVAL '2 day' AND csv_datetime < CURRENT_DATE - INTERVAL '1 day') combined_side_b_ng_avg_3, (SELECT avg(combined_side_a_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime >
-         CURRENT_DATE - INTERVAL '2 day' AND csv_datetime < CURRENT_DATE - INTERVAL '1 day') combined_side_a_re_avg_3, (SELECT avg(combined_side_b_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime >
-         CURRENT_DATE - INTERVAL '2 day' AND csv_datetime < CURRENT_DATE - INTERVAL '1 day') combined_side_b_re_avg_3,
 
--- query for three days ago
-(SELECT avg(combined_side_a_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '3 day' AND csv_datetime < CURRENT_DATE - INTERVAL '2 day') combined_side_a_ng_avg_4,
-(SELECT avg(combined_side_b_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '3 day' AND csv_datetime < CURRENT_DATE - INTERVAL '2 day') combined_side_b_ng_avg_4,
-(SELECT avg(combined_side_a_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '3 day' AND csv_datetime < CURRENT_DATE - INTERVAL '2 day') combined_side_a_re_avg_4,
-(SELECT avg(combined_side_b_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '3 day' AND csv_datetime < CURRENT_DATE - INTERVAL '2 day') combined_side_b_re_avg_4,
+    combined_side_a_ng_avg_2 = models.DecimalField(max_digits=22, decimal_places=16)
+    combined_side_b_ng_avg_2 = models.DecimalField(max_digits=22, decimal_places=16)
+    combined_side_a_re_avg_2 = models.DecimalField(max_digits=22, decimal_places=16)
+    combined_side_b_re_avg_2 = models.DecimalField(max_digits=22, decimal_places=16)
 
--- query for four days ago
-(SELECT avg(combined_side_a_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '4 day' AND csv_datetime < CURRENT_DATE - INTERVAL '3 day') combined_side_a_ng_avg_5,
-(SELECT avg(combined_side_b_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '4 day' AND csv_datetime < CURRENT_DATE - INTERVAL '3 day') combined_side_b_ng_avg_5,
-(SELECT avg(combined_side_a_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '4 day' AND csv_datetime < CURRENT_DATE - INTERVAL '3 day') combined_side_a_re_avg_5,
-(SELECT avg(combined_side_b_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '4 day' AND csv_datetime < CURRENT_DATE - INTERVAL '3 day') combined_side_b_re_avg_5,
+    combined_side_a_ng_avg_3 = models.DecimalField(max_digits=22, decimal_places=16)
+    combined_side_b_ng_avg_3 = models.DecimalField(max_digits=22, decimal_places=16)
+    combined_side_a_re_avg_3 = models.DecimalField(max_digits=22, decimal_places=16)
+    combined_side_b_re_avg_3 = models.DecimalField(max_digits=22, decimal_places=16)
 
--- query for five days ago
-(SELECT avg(combined_side_a_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '5 day' AND csv_datetime < CURRENT_DATE - INTERVAL '4 day') combined_side_a_ng_avg_6,
-(SELECT avg(combined_side_b_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '5 day' AND csv_datetime < CURRENT_DATE - INTERVAL '4 day') combined_side_b_ng_avg_6,
-(SELECT avg(combined_side_a_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '5 day' AND csv_datetime < CURRENT_DATE - INTERVAL '4 day') combined_side_a_re_avg_6,
-(SELECT avg(combined_side_b_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '5 day' AND csv_datetime < CURRENT_DATE - INTERVAL '4 day') combined_side_b_re_avg_6,
+    combined_side_a_ng_avg_4 = models.DecimalField(max_digits=22, decimal_places=16)
+    combined_side_b_ng_avg_4 = models.DecimalField(max_digits=22, decimal_places=16)
+    combined_side_a_re_avg_4 = models.DecimalField(max_digits=22, decimal_places=16)
+    combined_side_b_re_avg_4 = models.DecimalField(max_digits=22, decimal_places=16)
 
--- query for six days ago
-(SELECT avg(combined_side_a_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '6 day' AND csv_datetime < CURRENT_DATE - INTERVAL '5 day') combined_side_a_ng_avg_7,
-(SELECT avg(combined_side_b_ng) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '6 day' AND csv_datetime < CURRENT_DATE - INTERVAL '5 day') combined_side_b_ng_avg_7,
-(SELECT avg(combined_side_a_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '6 day' AND csv_datetime < CURRENT_DATE - INTERVAL '5 day') combined_side_a_re_avg_7,
-(SELECT avg(combined_side_b_re) FROM dolcegusto_dolcegusto_table WHERE csv_datetime > CURRENT_DATE - INTERVAL '6 day' AND csv_datetime < CURRENT_DATE - INTERVAL '5 day') combined_side_b_re_avg_7
-"
-        )
-        today = cursor.fetchone()
-    return today
+    combined_side_a_ng_avg_5 = models.DecimalField(max_digits=22, decimal_places=16)
+    combined_side_b_ng_avg_5 = models.DecimalField(max_digits=22, decimal_places=16)
+    combined_side_a_re_avg_5 = models.DecimalField(max_digits=22, decimal_places=16)
+    combined_side_b_re_avg_5 = models.DecimalField(max_digits=22, decimal_places=16)
+
+    combined_side_a_ng_avg_6 = models.DecimalField(max_digits=22, decimal_places=16)
+    combined_side_b_ng_avg_6 = models.DecimalField(max_digits=22, decimal_places=16)
+    combined_side_a_re_avg_6 = models.DecimalField(max_digits=22, decimal_places=16)
+    combined_side_b_re_avg_6 = models.DecimalField(max_digits=22, decimal_places=16)
+
+    combined_side_a_ng_avg_7 = models.DecimalField(max_digits=22, decimal_places=16)
+    combined_side_b_ng_avg_7 = models.DecimalField(max_digits=22, decimal_places=16)
+    combined_side_a_re_avg_7 = models.DecimalField(max_digits=22, decimal_places=16)
+    combined_side_b_re_avg_7 = models.DecimalField(max_digits=22, decimal_places=16)
+
+    class Meta:
+        managed = False
+        db_table = 'public.dolcegusto_past_week'
+
+#
+#     # bypassing django model and querying SQL directely
+# def scrap_rate(self):
+#
+#     with connection.cursor() as cursor:
+#         cursor.execute(
+#         "query here"
+#         )
+#         today = cursor.fetchone()
+#     return today
 
 # SQL View query backup:
 # CREATE OR REPLACE VIEW past_week (
