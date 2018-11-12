c66ed868e8e736a00e6fd9d11e4903d76a1b4a36
Peter M <petermogyoros@gmail.com>
2018-11-12 17:11:02 +0000

    Added another line to the database

--- dolcegusto/views.py
+++ dolcegusto/views.py
@@ -40,7 +40,7 @@ class Line(View):
         day6 = get_weekday(datetime.strptime(str(daily_report(5).production_day[6])[0:10], '%Y-%m-%d').date().weekday)
 
 
-        def render_per_line(line5, line8):
+        def render_per_line(line5, line8, line9):
 
             # get combined reject rate for the whole machine. Rejects for each side + recycles for each side
             # if one side is not running, do not devide final vale by 2
@@ -124,33 +124,84 @@ class Line(View):
             else:
                 print("Machine Not Running")
 
+
+            if line9 == 9:
+                line = 9
+                if daily_report(line).combined_side_a_ng[0] < 0.1 or daily_report(line).combined_side_b_ng[0] < 0.1:
+                    combined_scrap_0_line_9 = daily_report(line).combined_side_a_ng[0] + daily_report(line).combined_side_b_ng[0] + daily_report(line).combined_side_a_re[0] + daily_report(line).combined_side_b_re[0]
+                elif daily_report(line).combined_side_a_ng[0] > 0.1 and daily_report(line).combined_side_b_ng[0] > 0.1:
+                    combined_scrap_0_line_9 = (daily_report(line).combined_side_a_ng[0] + daily_report(line).combined_side_b_ng[0] + daily_report(line).combined_side_a_re[0] + daily_report(line).combined_side_b_re[0]) / 2
+
+                if daily_report(line).combined_side_a_ng[1] < 0.1 or daily_report(line).combined_side_b_ng[1] < 0.1:
+                    combined_scrap_1_line_9 = daily_report(line).combined_side_a_ng[1] + daily_report(line).combined_side_b_ng[1] + daily_report(line).combined_side_a_re[1] + daily_report(line).combined_side_b_re[1]
+                elif daily_report(line).combined_side_a_ng[1] > 0.1 and daily_report(line).combined_side_b_ng[1] > 0.1:
+                    combined_scrap_1_line_9 = (daily_report(line).combined_side_a_ng[1] + daily_report(line).combined_side_b_ng[1] + daily_report(line).combined_side_a_re[1] + daily_report(line).combined_side_b_re[1]) / 2
+
+                if daily_report(line).combined_side_a_ng[2] < 0.1 or daily_report(line).combined_side_b_ng[2] < 0.1:
+                    combined_scrap_2_line_9 = daily_report(line).combined_side_a_ng[2] + daily_report(line).combined_side_b_ng[2] + daily_report(line).combined_side_a_re[2] + daily_report(line).combined_side_b_re[2]
+                elif daily_report(line).combined_side_a_ng[2] > 0.1 and daily_report(line).combined_side_b_ng[2] > 0.1:
+                    combined_scrap_2_line_9 = (daily_report(line).combined_side_a_ng[2] + daily_report(line).combined_side_b_ng[2] + daily_report(line).combined_side_a_re[2] + daily_report(line).combined_side_b_re[2]) / 2
+
+                if daily_report(line).combined_side_a_ng[3] < 0.1 or daily_report(line).combined_side_b_ng[3] < 0.1:
+                    combined_scrap_3_line_9 = daily_report(line).combined_side_a_ng[3] + daily_report(line).combined_side_b_ng[3] + daily_report(line).combined_side_a_re[3] + daily_report(line).combined_side_b_re[3]
+                elif daily_report(line).combined_side_a_ng[3] > 0.1 and daily_report(line).combined_side_b_ng[3] > 0.1:
+                    combined_scrap_3_line_9 = (daily_report(line).combined_side_a_ng[3] + daily_report(line).combined_side_b_ng[3] + daily_report(line).combined_side_a_re[3] + daily_report(line).combined_side_b_re[3]) / 2
+
+                if daily_report(line).combined_side_a_ng[4] < 0.1 or daily_report(line).combined_side_b_ng[4] < 0.1:
+                    combined_scrap_4_line_9 = daily_report(line).combined_side_a_ng[4] + daily_report(line).combined_side_b_ng[4] + daily_report(line).combined_side_a_re[4] + daily_report(line).combined_side_b_re[4]
+                elif daily_report(line).combined_side_a_ng[4] > 0.1 and daily_report(line).combined_side_b_ng[4] > 0.1:
+                    combined_scrap_4_line_9 = (daily_report(line).combined_side_a_ng[4] + daily_report(line).combined_side_b_ng[4] + daily_report(line).combined_side_a_re[4] + daily_report(line).combined_side_b_re[4]) / 2
+
+                if daily_report(line).combined_side_a_ng[5] < 0.1 or daily_report(line).combined_side_b_ng[5] < 0.1:
+                    combined_scrap_5_line_9 = daily_report(line).combined_side_a_ng[5] + daily_report(line).combined_side_b_ng[5] + daily_report(line).combined_side_a_re[5] + daily_report(line).combined_side_b_re[5]
+                elif daily_report(line).combined_side_a_ng[5] > 0.1 and daily_report(line).combined_side_b_ng[5] > 0.1:
+                    combined_scrap_5_line_9 = (daily_report(line).combined_side_a_ng[5] + daily_report(line).combined_side_b_ng[5] + daily_report(line).combined_side_a_re[5] + daily_report(line).combined_side_b_re[5]) / 2
+
+                if daily_report(line).combined_side_a_ng[6] < 0.1 or daily_report(line).combined_side_b_ng[6] < 0.1:
+                    combined_scrap_6_line_9 = daily_report(line).combined_side_a_ng[6] + daily_report(line).combined_side_b_ng[6] + daily_report(line).combined_side_a_re[6] + daily_report(line).combined_side_b_re[6]
+                elif daily_report(line).combined_side_a_ng[6] > 0.1 and daily_report(line).combined_side_b_ng[6] > 0.1:
+                    combined_scrap_6_line_9 = (daily_report(line).combined_side_a_ng[6] + daily_report(line).combined_side_b_ng[6] + daily_report(line).combined_side_a_re[6] + daily_report(line).combined_side_b_re[6]) / 2
+
+            else:
+                print("Machine Not Running")
+
+
+
             return {
             "line_5": daily_report(5).line[0],
             "line_8": daily_report(8).line[0],
+            "line_9": daily_report(9).line[0],
             "day0": day0,
             "combined_scrap_0_line_5": combined_scrap_0_line_5,
             "combined_scrap_0_line_8": combined_scrap_0_line_8,
+            "combined_scrap_0_line_9": combined_scrap_0_line_9,
             "day1": day1,
             "combined_scrap_1_line_5": combined_scrap_1_line_5,
             "combined_scrap_1_line_8": combined_scrap_1_line_8,
+            "combined_scrap_1_line_9": combined_scrap_1_line_9,
             "day2": day2,
             "combined_scrap_2_line_5": combined_scrap_2_line_5,
             "combined_scrap_2_line_8": combined_scrap_2_line_8,
+            "combined_scrap_2_line_9": combined_scrap_2_line_9,
             "day3": day3,
             "combined_scrap_3_line_5": combined_scrap_3_line_5,
             "combined_scrap_3_line_8": combined_scrap_3_line_8,
+            "combined_scrap_3_line_9": combined_scrap_3_line_9,
             "day4": day4,
             "combined_scrap_4_line_5": combined_scrap_4_line_5,
             "combined_scrap_4_line_8": combined_scrap_4_line_8,
+            "combined_scrap_4_line_9": combined_scrap_4_line_9,
             "day5": day5,
             "combined_scrap_5_line_5": combined_scrap_5_line_5,
             "combined_scrap_5_line_8": combined_scrap_5_line_8,
+            "combined_scrap_5_line_9": combined_scrap_5_line_9,
             "day6": day6,
             "combined_scrap_6_line_5": combined_scrap_6_line_5,
-            "combined_scrap_6_line_8": combined_scrap_6_line_8
+            "combined_scrap_6_line_8": combined_scrap_6_line_8,
+            "combined_scrap_6_line_9": combined_scrap_6_line_9
                     }
 
 
-        lines = render_per_line(5, 8)
+        lines = render_per_line(5, 8, 9)
 
         return render(request, "dolcegusto/line.html", lines)
