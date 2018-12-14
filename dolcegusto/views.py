12dfed75e8dfcce52e33ceeb94b1f1e1ac60f823
MOGYOROS <MogyorosP@GCS.COM>
2018-12-14 16:12:46 +0000

    Extended new template to individual lines

--- dolcegusto/views.py
+++ dolcegusto/views.py
@@ -226,232 +226,50 @@ class Table(View):
         "combined_scrap_6_10_a": line10["day6_a"],
         "combined_scrap_6_10_b": line10["day6_b"],
         })
+#
+# class Line3(View):
+#     model = hourly_report
+#     def get(self, request):
+#         print("Hello")
+#
 
-        # line3 = past_seven_days(3)
-        #combined_scrap_0_line_3 = line3["combined_scrap_0"]
-        # combined_scrap_1_line_3 = line3["combined_scrap_1"]
-        # combined_scrap_2_line_3 = line3["combined_scrap_2"]
-        # combined_scrap_3_line_3 = line3["combined_scrap_3"]
-        # combined_scrap_4_line_3 = line3["combined_scrap_4"]
-        # combined_scrap_5_line_3 = line3["combined_scrap_5"]
-        # combined_scrap_6_line_3 = line3["combined_scrap_6"]
-        line4 = assign_variables(past_seven_days(4))
+class Line3(View):
+    model = daily_report
+    def get(self, request):
 
-        return render(request, "dolcegusto/line4.html", {
-        "line_3": 3,
-        "line_4": 4,
-        "line_5": 5,
-        "line_7": 7,
-        "line_8": 8,
-        "line_9": 9,
-        "line_10": 10,
+        # This gives the function above an integer which translates to the weekday name
+        day2 = get_weekday(datetime.strptime(str(daily_report(5).production_day[2])[0:10], '%Y-%m-%d').date().weekday)
+        day3 = get_weekday(datetime.strptime(str(daily_report(5).production_day[3])[0:10], '%Y-%m-%d').date().weekday)
+        day4 = get_weekday(datetime.strptime(str(daily_report(5).production_day[4])[0:10], '%Y-%m-%d').date().weekday)
+        day5 = get_weekday(datetime.strptime(str(daily_report(5).production_day[5])[0:10], '%Y-%m-%d').date().weekday)
+        day6 = get_weekday(datetime.strptime(str(daily_report(5).production_day[6])[0:10], '%Y-%m-%d').date().weekday)
+
+        line3 = assign_variables(past_seven_days(3))
+
+        return render(request, "dolcegusto/line3.html", {
+        "line": 3,
+        "day0":"Today",
+        "day1":"Yesterday",
         "day2": day2,
         "day3": day3,
         "day4": day4,
         "day5": day5,
         "day6": day6,
-        "combined_scrap_0_a": line4["day0_a"],
-        "combined_scrap_0_b": line4["day0_b"],
-        "combined_scrap_1_a": line4["day1_a"],
-        "combined_scrap_1_b": line4["day1_b"],
-        "combined_scrap_2_a": line4["day2_a"],
-        "combined_scrap_2_b": line4["day2_b"],
-        "combined_scrap_3_a": line4["day3_a"],
-        "combined_scrap_3_b": line4["day3_b"],
-        "combined_scrap_4_a": line4["day4_a"],
-        "combined_scrap_4_b": line4["day4_b"],
-        "combined_scrap_5_a": line4["day5_a"],
-        "combined_scrap_5_b": line4["day5_b"],
-        "combined_scrap_6_a": line4["day6_a"],
-        "combined_scrap_6_b": line4["day6_b"]
+        "combined_scrap_0_a": line3["day0_a"],
+        "combined_scrap_0_b": line3["day0_b"],
+        "combined_scrap_1_a": line3["day1_a"],
+        "combined_scrap_1_b": line3["day1_b"],
+        "combined_scrap_2_a": line3["day2_a"],
+        "combined_scrap_2_b": line3["day2_b"],
+        "combined_scrap_3_a": line3["day3_a"],
+        "combined_scrap_3_b": line3["day3_b"],
+        "combined_scrap_4_a": line3["day4_a"],
+        "combined_scrap_4_b": line3["day4_b"],
+        "combined_scrap_5_a": line3["day5_a"],
+        "combined_scrap_5_b": line3["day5_b"],
+        "combined_scrap_6_a": line3["day6_a"],
+        "combined_scrap_6_b": line3["day6_b"]
         })
-
-        # # This gives the function above an integer which translates to the weekday name
-        # day0 = get_weekday(datetime.strptime(str(daily_report(5).production_day[0])[0:10], '%Y-%m-%d').date().weekday)
-        # day1 = get_weekday(datetime.strptime(str(daily_report(5).production_day[1])[0:10], '%Y-%m-%d').date().weekday)
-        # day2 = get_weekday(datetime.strptime(str(daily_report(5).production_day[2])[0:10], '%Y-%m-%d').date().weekday)
-        # day3 = get_weekday(datetime.strptime(str(daily_report(5).production_day[3])[0:10], '%Y-%m-%d').date().weekday)
-        # day4 = get_weekday(datetime.strptime(str(daily_report(5).production_day[4])[0:10], '%Y-%m-%d').date().weekday)
-        # day5 = get_weekday(datetime.strptime(str(daily_report(5).production_day[5])[0:10], '%Y-%m-%d').date().weekday)
-        # day6 = get_weekday(datetime.strptime(str(daily_report(5).production_day[6])[0:10], '%Y-%m-%d').date().weekday)
-        #
-        # render_dictionary = {}
-        # line3 = assign_variables(past_seven_days(3))
-        # combined_scrap_0_line_3 = line3["combined_scrap_0"]
-        # combined_scrap_1_line_3 = line3["combined_scrap_1"]
-        # combined_scrap_2_line_3 = line3["combined_scrap_2"]
-        # combined_scrap_3_line_3 = line3["combined_scrap_3"]
-        # combined_scrap_4_line_3 = line3["combined_scrap_4"]
-        # combined_scrap_5_line_3 = line3["combined_scrap_5"]
-        # combined_scrap_6_line_3 = line3["combined_scrap_6"]
-        #
-        #
-        # line4 = assign_variables(past_seven_days(4))
-        # combined_scrap_0_line_4 = line4["combined_scrap_0"]
-        # combined_scrap_1_line_4 = line4["combined_scrap_1"]
-        # combined_scrap_2_line_4 = line4["combined_scrap_2"]
-        # combined_scrap_3_line_4 = line4["combined_scrap_3"]
-        # combined_scrap_4_line_4 = line4["combined_scrap_4"]
-        # combined_scrap_5_line_4 = line4["combined_scrap_5"]
-        # combined_scrap_6_line_4 = line4["combined_scrap_6"]
-        #
-        #
-        # line5 = assign_variables(past_seven_days(5))
-        # combined_scrap_0_line_5 = line5["combined_scrap_0"]
-        # combined_scrap_1_line_5 = line5["combined_scrap_1"]
-        # combined_scrap_2_line_5 = line5["combined_scrap_2"]
-        # combined_scrap_3_line_5 = line5["combined_scrap_3"]
-        # combined_scrap_4_line_5 = line5["combined_scrap_4"]
-        # combined_scrap_5_line_5 = line5["combined_scrap_5"]
-        # combined_scrap_6_line_5 = line5["combined_scrap_6"]
-        #
-        #
-        # line7 = assign_variables(past_seven_days(7))
-        # combined_scrap_0_line_7 = line7["combined_scrap_0"]
-        # combined_scrap_1_line_7 = line7["combined_scrap_1"]
-        # combined_scrap_2_line_7 = line7["combined_scrap_2"]
-        # combined_scrap_3_line_7 = line7["combined_scrap_3"]
-        # combined_scrap_4_line_7 = line7["combined_scrap_4"]
-        # combined_scrap_5_line_7 = line7["combined_scrap_5"]
-        # combined_scrap_6_line_7 = line7["combined_scrap_6"]
-        #
-        #
-        # line8 = assign_variables(past_seven_days(8))
-        # combined_scrap_0_line_8 = line8["combined_scrap_0"]
-        # combined_scrap_1_line_8 = line8["combined_scrap_1"]
-        # combined_scrap_2_line_8 = line8["combined_scrap_2"]
-        # combined_scrap_3_line_8 = line8["combined_scrap_3"]
-        # combined_scrap_4_line_8 = line8["combined_scrap_4"]
-        # combined_scrap_5_line_8 = line8["combined_scrap_5"]
-        # combined_scrap_6_line_8 = line8["combined_scrap_6"]
-        #
-        #
-        # line9 = assign_variables(past_seven_days(9))
-        # combined_scrap_0_line_9 = line9["combined_scrap_0"]
-        # combined_scrap_1_line_9 = line9["combined_scrap_1"]
-        # combined_scrap_2_line_9 = line9["combined_scrap_2"]
-        # combined_scrap_3_line_9 = line9["combined_scrap_3"]
-        # combined_scrap_4_line_9 = line9["combined_scrap_4"]
-        # combined_scrap_5_line_9 = line9["combined_scrap_5"]
-        # combined_scrap_6_line_9 = line9["combined_scrap_6"]
-        #
-        #
-        # line10 = assign_variables(past_seven_days(10))
-        # combined_scrap_0_line_10 = line10["combined_scrap_0"]
-        # combined_scrap_1_line_10 = line10["combined_scrap_1"]
-        # combined_scrap_2_line_10 = line10["combined_scrap_2"]
-        # combined_scrap_3_line_10 = line10["combined_scrap_3"]
-        # combined_scrap_4_line_10 = line10["combined_scrap_4"]
-        # combined_scrap_5_line_10 = line10["combined_scrap_5"]
-        # combined_scrap_6_line_10 = line10["combined_scrap_6"]
-        #
-        # return render(request, "dolcegusto/scrap_rate.html", {
-        # "line_3": 3,
-        # "line_4": 4,
-        # "line_5": 5,
-        # "line_7": 7,
-        # "line_8": 8,
-        # "line_9": 9,
-        # "line_10": 10,
-        # "combined_scrap_0_line_3": combined_scrap_0_line_3,
-        # "combined_scrap_0_line_4": combined_scrap_0_line_4,
-        # "combined_scrap_0_line_5": combined_scrap_0_line_5,
-        # "combined_scrap_0_line_7": combined_scrap_0_line_7,
-        # "combined_scrap_0_line_8": combined_scrap_0_line_8,
-        # "combined_scrap_0_line_9": combined_scrap_0_line_9,
-        # "combined_scrap_0_line_10": combined_scrap_0_line_10,
-        # "day1": day1,
-        # "combined_scrap_1_line_3": combined_scrap_1_line_3,
-        # "combined_scrap_1_line_4": combined_scrap_1_line_4,
-        # "combined_scrap_1_line_5": combined_scrap_1_line_5,
-        # "combined_scrap_1_line_7": combined_scrap_1_line_7,
-        # "combined_scrap_1_line_8": combined_scrap_1_line_8,
-        # "combined_scrap_1_line_9": combined_scrap_1_line_9,
-        # "combined_scrap_1_line_10": combined_scrap_1_line_10,
-        # "day2": day2,
-        # "combined_scrap_2_line_3": combined_scrap_2_line_3,
-        # "combined_scrap_2_line_4": combined_scrap_2_line_4,
-        # "combined_scrap_2_line_5": combined_scrap_2_line_5,
-        # "combined_scrap_2_line_7": combined_scrap_2_line_7,
-        # "combined_scrap_2_line_8": combined_scrap_2_line_8,
-        # "combined_scrap_2_line_9": combined_scrap_2_line_9,
-        # "combined_scrap_2_line_10": combined_scrap_2_line_10,
-        # "day3": day3,
-        # "combined_scrap_3_line_3": combined_scrap_3_line_3,
-        # "combined_scrap_3_line_4": combined_scrap_3_line_4,
-        # "combined_scrap_3_line_5": combined_scrap_3_line_5,
-        # "combined_scrap_3_line_7": combined_scrap_3_line_7,
-        # "combined_scrap_3_line_8": combined_scrap_3_line_8,
-        # "combined_scrap_3_line_9": combined_scrap_3_line_9,
-        # "combined_scrap_3_line_10": combined_scrap_3_line_10,
-        # "day4": day4,
-        # "combined_scrap_4_line_3": combined_scrap_4_line_3,
-        # "combined_scrap_4_line_4": combined_scrap_4_line_4,
-        # "combined_scrap_4_line_5": combined_scrap_4_line_5,
-        # "combined_scrap_4_line_7": combined_scrap_4_line_7,
-        # "combined_scrap_4_line_8": combined_scrap_4_line_8,
-        # "combined_scrap_4_line_9": combined_scrap_4_line_9,
-        # "combined_scrap_4_line_10": combined_scrap_4_line_10,
-        # "day5": day5,
-        # "combined_scrap_5_line_3": combined_scrap_5_line_3,
-        # "combined_scrap_5_line_4": combined_scrap_5_line_4,
-        # "combined_scrap_5_line_5": combined_scrap_5_line_5,
-        # "combined_scrap_5_line_7": combined_scrap_5_line_7,
-        # "combined_scrap_5_line_8": combined_scrap_5_line_8,
-        # "combined_scrap_5_line_9": combined_scrap_5_line_9,
-        # "combined_scrap_5_line_10": combined_scrap_5_line_10,
-        # "day6": day6,
-        # "combined_scrap_6_line_3": combined_scrap_6_line_3,
-        # "combined_scrap_6_line_4": combined_scrap_6_line_4,
-        # "combined_scrap_6_line_5": combined_scrap_6_line_5,
-        # "combined_scrap_6_line_7": combined_scrap_6_line_7,
-        # "combined_scrap_6_line_8": combined_scrap_6_line_8,
-        # "combined_scrap_6_line_9": combined_scrap_6_line_9,
-        # "combined_scrap_6_line_10": combined_scrap_6_line_10
-        # })
-
-
-class Line3(View):
-    model = hourly_report
-    def get(self, request):
-        print("Hello")
-
-
-# class Line3(View):
-    # model = daily_report
-    # def get(self, request):
-    #
-    #     # This gives the function above an integer which translates to the weekday name
-    #     day2 = get_weekday(datetime.strptime(str(daily_report(5).production_day[2])[0:10], '%Y-%m-%d').date().weekday)
-    #     day3 = get_weekday(datetime.strptime(str(daily_report(5).production_day[3])[0:10], '%Y-%m-%d').date().weekday)
-    #     day4 = get_weekday(datetime.strptime(str(daily_report(5).production_day[4])[0:10], '%Y-%m-%d').date().weekday)
-    #     day5 = get_weekday(datetime.strptime(str(daily_report(5).production_day[5])[0:10], '%Y-%m-%d').date().weekday)
-    #     day6 = get_weekday(datetime.strptime(str(daily_report(5).production_day[6])[0:10], '%Y-%m-%d').date().weekday)
-    #
-    #     line3 = assign_variables(get_value_for_machine_per_period(3))
-    #
-    #     return render(request, "dolcegusto/line3.html", {
-    #     "line": 3,
-    #     "day2": day2,
-    #     "day3": day3,
-    #     "day4": day4,
-    #     "day5": day5,
-    #     "day6": day6,
-    #     "combined_scrap_0_a": line3["day0_a"],
-    #     "combined_scrap_0_b": line3["day0_b"],
-    #     "combined_scrap_1_a": line3["day1_a"],
-    #     "combined_scrap_1_b": line3["day1_b"],
-    #     "combined_scrap_2_a": line3["day2_a"],
-    #     "combined_scrap_2_b": line3["day2_b"],
-    #     "combined_scrap_3_a": line3["day3_a"],
-    #     "combined_scrap_3_b": line3["day3_b"],
-    #     "combined_scrap_4_a": line3["day4_a"],
-    #     "combined_scrap_4_b": line3["day4_b"],
-    #     "combined_scrap_5_a": line3["day5_a"],
-    #     "combined_scrap_5_b": line3["day5_b"],
-    #     "combined_scrap_6_a": line3["day6_a"],
-    #     "combined_scrap_6_b": line3["day6_b"]
-    #     })
 
 class Line4(View):
     model = daily_report
